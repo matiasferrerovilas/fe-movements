de9340e5c993501cbf4a7fe7037f9cceb544286b
matiasferrerovilas <matigfv@gmail.com>
2026-03-23 22:13:26 +0100

    Cambio a subscription el service de  Servicios

--- src/apis/ServiceApi.tsx
+++ src/apis/ServiceApi.tsx
@@ -1,6 +1,9 @@
+
 import type { CurrencyRecord } from "../models/Currency";
 import type { Service, ServiceToUpdate } from "../models/Service";
 import { api } from "./axios";
+
+const BASE_PATH = "/subscriptions";
 
 export interface ServiceToAdd {
   amount: number;
@@ -12,48 +15,48 @@ export interface ServiceToAdd {
 }
 export async function getServicesApi() {
   return api
-    .get<Service[]>("/services")
+    .get<Service[]>(BASE_PATH)
     .then((response) => response.data)
     .catch((error) => {
-      console.error("Error fetching services:", error);
+      console.error("Error fetching subscriptions:", error);
       throw error;
     });
 }
 
 export async function payServiceApi(service: Service) {
   return api
-    .patch("/services/" + service.id + "/payment")
+    .patch(`${BASE_PATH}/` + service.id + "/payment")
     .then((response) => response.data)
     .catch((error) => {
-      console.error("Error updating services:", error);
+      console.error("Error updating subscriptions:", error);
       throw error;
     });
 }
 export async function updateServiceApi(serviceToUpdate: ServiceToUpdate) {
   return api
-    .patch("/services/" + serviceToUpdate.id, serviceToUpdate.changes)
+    .patch(`${BASE_PATH}/` + serviceToUpdate.id, serviceToUpdate.changes)
     .then((response) => response.data)
     .catch((error) => {
-      console.error("Error updating services:", error);
+      console.error("Error updating subscriptions:", error);
       throw error;
     });
 }
 export async function addServiceApi(service: ServiceToAdd) {
   return api
-    .post("/services", service)
+    .post(BASE_PATH, service)
     .then((response) => response.data)
     .catch((error) => {
-      console.error("Error updating services:", error);
+      console.error("Error updating subscriptions:", error);
       throw error;
     });
 }
 
 export async function deleteServiceApi(service: Service) {
   return api
-    .delete("/services/" + service.id)
+    .delete(`${BASE_PATH}/` + service.id)
     .then((response) => response.data)
     .catch((error) => {
-      console.error("Error updating services:", error);
+      console.error("Error updating subscriptions:", error);
       throw error;
     });
 }
